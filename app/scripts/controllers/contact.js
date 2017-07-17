d766ed409e5944d01a34ee11111c4ff99fcd82d7
Oscar Pérez <mr.omiguelperez@gmail.com>
2017-07-17 16:02:51 -0500

    Página de contactos
    - Controlador, ruta y vista

---
+++ app/scripts/controllers/contact.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularChartjsSampleApp.controller:ContactCtrl
+ * @description
+ * # ContactCtrl
+ * Controller of the angularChartjsSampleApp
+ */
+angular.module('angularChartjsSampleApp')
+  .controller('ContactCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
