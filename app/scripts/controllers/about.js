534df5a3e5f165618ff9439b94ff7040195de853
Oscar Pérez <mr.omiguelperez@gmail.com>
2017-07-17 15:43:37 -0500

    Base del proyecto

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularChartjsSampleApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the angularChartjsSampleApp
+ */
+angular.module('angularChartjsSampleApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularChartjsSampleApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angularChartjsSampleApp
+ */
+angular.module('angularChartjsSampleApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
